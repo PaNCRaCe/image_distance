c17260c6f5e2052b26df9494730da63124fc39bd
Bastien <boubabeast@gmail.com>
2019-02-18 03:33:28 +0100

    correction du style et modif des commentaires

--- image_distance.py
+++ image_distance.py
@@ -6,30 +6,38 @@ import numpy as np
 from skimage import draw
 import matplotlib.pyplot as plt
 
+
 def _sqr_distance_pt(pt, grid_y, grid_x):
     """
     calcul du carré de la distance à un point pour chaque pixel
     """
     return (grid_y - pt[0]) ** 2 + (grid_x - pt[1]) ** 2
 
+
 def distance_segment(pt0, pt1, image_shape):
     """
-    calcul de la distance minimale (en pixels) de chaque pixel de l'image à un segment
+    calcul de la distance minimale à un segment (en pixels) pour
+    chaque pixel de l'image
+    - pt0, pt1 : extrémités du segment
+    - image_shape : dimensions de l'image
+
+    resultat : image (float)
     """
 
     i_rows, i_cols = draw.line(pt0[1], pt0[0], pt1[1], pt1[0])
     resultat = np.full(image_shape, np.inf)
     height, width = image_shape
     grid_y, grid_x = np.mgrid[0:height, 0:width]
-    
+
     for pt in zip(i_rows, i_cols):
         image_dist = _sqr_distance_pt(pt, grid_y, grid_x)
         resultat[resultat > image_dist] = image_dist[resultat > image_dist]
-    
+
     return np.sqrt(resultat)
 
+
 if __name__ == "__main__":
-    
+
     I_shape = (50, 100)
     segment_0 = (40, 5)
     segment_1 = (5, 75)
@@ -38,4 +46,3 @@ if __name__ == "__main__":
     plt.figure()
     plt.imshow(resultat)
     plt.show()
-
