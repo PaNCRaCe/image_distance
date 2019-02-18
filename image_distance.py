6048adbf9bafc7363d3fc7ce65a4ae22a2dd30c4
Bastien <boubabeast@gmail.com>
2019-02-18 03:48:06 +0100

    ajout d'une colorbar et d'un titre

--- image_distance.py
+++ image_distance.py
@@ -42,10 +42,13 @@ def distance_segment(pt0, pt1, image_shape):
 if __name__ == "__main__":
 
     I_shape = (50, 100)
-    segment_0 = (40, 5)
-    segment_1 = (5, 75)
+    segment_0 = (60, 5)
+    segment_1 = (20, 40)
     resultat = distance_segment(segment_0, segment_1, I_shape)
 
     plt.figure()
     plt.imshow(resultat)
+    plt.colorbar()
+    plt.title("distance au segment %s - %s" % (segment_0, segment_1))
     plt.show()
+
