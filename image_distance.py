18a43c9beaa4723e9516ce581f36dd88a5364f8d
Bastien <boubabeast@gmail.com>
2019-02-18 03:03:33 +0100

    ajout d'un main et de la fonction distance_segment. renommage de distance en distance_pt

--- image_distance.py
+++ image_distance.py
@@ -6,18 +6,27 @@ import numpy as np
 from skimage import draw
 import matplotlib.pyplot as plt
 
-def distance(pt, grid_y, grid_x):
+def distance_pt(pt, grid_y, grid_x):
     return np.sqrt((grid_y - pt[0]) ** 2 + (grid_x - pt[1]) ** 2)
 
-if __name__ == "__main__":
-    i_rows, i_cols = draw.line(40, 5, 5, 75)
+def distance_segment(pt0, pt1, image_shape):
+    i_rows, i_cols = draw.line(pt0[1], pt0[0], pt1[1], pt1[0])
     resultat = np.full((50, 100), np.inf)
     height, width = resultat.shape
     grid_y, grid_x = np.mgrid[0:height, 0:width]
     
     for pt in zip(i_rows, i_cols):
-        image_dist = distance(pt, grid_y, grid_x)
+        image_dist = distance_pt(pt, grid_y, grid_x)
         resultat[resultat > image_dist] = image_dist[resultat > image_dist]
+    
+    return resultat
+
+if __name__ == "__main__":
+    
+    I_shape = (50, 100)
+    segment_0 = (40, 5)
+    segment_1 = (5, 75)
+    resultat = distance_segment(segment_0, segment_1, I_shape)
 
     plt.figure()
     plt.imshow(resultat)
